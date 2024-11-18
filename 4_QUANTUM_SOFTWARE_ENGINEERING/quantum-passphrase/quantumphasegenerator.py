0870d3653bbb5613ffbe79f10bec9186ee3fba4c
AlvaroMAMunex <amapamor@unex.es>
2024-11-18 20:20:48 +0100

    quantum phase generator

--- 4_QUANTUM_SOFTWARE_ENGINEERING/quantum-passphrase/quantumphasegenerator.py
+++ 4_QUANTUM_SOFTWARE_ENGINEERING/quantum-passphrase/quantumphasegenerator.py
@@ -1,7 +1,9 @@
 from flask import Response, request, render_template, jsonify, redirect, url_for
+from braket.circuits import Circuit
+from braket.aws import AwsDevice
+from braket.devices.local_simulator import LocalSimulator
 import json
 import requests
-
 
 app = Flask(__name__)
 
@@ -22,21 +24,39 @@ def quantumphasegenerator():
         if not data_recieved or 'size' not in data_recieved:
             return jsonify({"error": "Falta el parámetro 'size' en el cuerpo de la solicitud"}), 400
         else:
-            length = data_recieved['size']
 
-            # Construcción de algoritmo cuántico y lanzamiento a braket
-                # Bucle para añadir puertas hadamard
+            length = int(data_recieved['size'])
             #Obtención de resultados de braket
-            braket_result = None
+            device = LocalSimulator()
+            #device = AwsDevice()
+            circuit = Circuit().h(range(length))
+            braket_result = device.run(circuit, shots=100).result()
+            measurement = list(braket_result.measurements[0])
+            generated_string = ''.join(map(str, measurement))
+            word_binary_index = []
+            word = ""
+            for bit in generated_string:
+                if len(word) < 8:
+                    word = word + bit # Concateno los bits para formar palabras de 8 bits, cada 8 bits representará un número en decimal
+                else:
+                    word_binary_index.append(word)
+                    word = "" + bit # Iniciamos nueva palabra
+            print(word_binary_index)
             #Request a data-muse
             data_muse_request = {
                 'rel_jja' : 'common',
-                'max' : braket_result
+                'max' : 128
             }
             response = requests.post(DATA_MUSE_API, json=data_muse_request)
             if response.status_code == 200:
-                words  = [word['word'] for word in response.json()]
-                return words
+                muse_word = response.json()
+                generated_phrase = ""
+                for binary_index in word_binary_index:
+                    # tranformación de binario a decimal
+                    decimal_index = int(binary_index,2)
+                    # tomo palabra del índice
+                    generated_phrase = generated_phrase + muse_word[decimal_index]['word']
+                return generated_phrase
     else:
         return jsonify({"error": "WRONG REQUEST"}), 400
 
